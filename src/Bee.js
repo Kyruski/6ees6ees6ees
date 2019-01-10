01f5bc8492dd9e634b10287efb213e59c05999dd
kyruski <jamescboyett@gmail.com>
2019-01-09 18:53:38 -0600

    Implements all ES6 classes for 6ees6ees6ees

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,9 @@
-class Bee {
-  // TODO..
+class Bee extends Grub{
+
+  constructor(age, color, food) {
+    super();
+    this.age = 5;
+    this.color = 'yellow';
+    this.job = 'Keep on growing';
+  }
 };
--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,17 @@
-class HoneyMakerBee {
-  // TODO..
+class HoneyMakerBee extends Bee{
+  
+  constructor(age, color, job, food) {
+    super();
+    this.age = 10;
+    this.job = 'make honey';
+    this.honeyPot = 0;
+  }
+
+  makeHoney() {
+    this.honeyPot++;
+  }
+
+  giveHoney() {
+    this.honeyPot--;
+  }
 };
